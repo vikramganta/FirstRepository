8590b56249404c388b161838bef80ce27ef3ad35
S525907 <S525907@NW36337.nwmissouri.edu>
2016-09-08 11:07:49 -0500

    Bearcat is changed to BearcatVikram

--- src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -37,7 +37,7 @@ package com.toy.anagrams.lib;
 final class StaticWordLibrary extends WordLibrary {
 
     private static final String[] WORD_LIST = {
-        "bearcat",
+        "bearcatvikram",
         "abstraction",
         "ambiguous",
         "arithmetic",
@@ -85,7 +85,7 @@ final class StaticWordLibrary extends WordLibrary {
         "traditional"};
 
     private static final String[] SCRAMBLED_WORD_LIST = {
-        "catbear",
+        "catbearmarkim",
         "batsartcoin",
         "maibuguos",
         "ratimhteci",
